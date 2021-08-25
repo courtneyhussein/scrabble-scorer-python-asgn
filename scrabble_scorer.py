eac0624e9c20158ff36827b6a885288f52ad46e4
Courtney Hussein <81171381+courtneyhussein@users.noreply.github.com>
2021-08-25 09:41:58 -0400

    Initial commit

---
+++ scrabble_scorer.py
@@ -0,0 +1,52 @@
+# inspired by https://exercism.io/tracks/javascript/exercises/etl/solutions/91f99a3cca9548cebe5975d7ebca6a85
+
+
+OLD_POINT_STRUCTURE = {
+  1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+  2: ['D', 'G'],
+  3: ['B', 'C', 'M', 'P'],
+  4: ['F', 'H', 'V', 'W', 'Y'],
+  5: ['K'],
+  8: ['J', 'X'],
+  10: ['Q', 'Z']
+}
+
+def old_scrabble_scorer(word):
+    word = word.upper()
+    letterPoints = ""
+
+    for char in word:
+
+        for point_value in old_point_structure:
+
+            if char in old_point_structure[point_value]:
+                letterPoints += 'Points for {char}: {point_value}\n'.format(char = char, point_value = point_value)
+
+    return letterPoints
+
+# your job is to finish writing these functions and variables that we've named
+# don't change the names or your program won't work as expected.
+
+def initial_prompt():
+   print("Let's play some Scrabble!\n")
+
+
+def simple_scorer():
+   return 
+
+def vowel_bonus_scorer():
+    return 
+
+def scrabble_scorer():
+    return
+
+scoring_algorithms = ()
+
+def scorer_prompt():
+    return 
+
+def transform():
+    return
+
+def run_program():
+    word = initial_prompt()
